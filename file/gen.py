838e3a8bb4ecaa997d7359f15032366a421afd6c
santosh kumar mehra <santoshkumarmehra99@gmail.com>
2022-10-04 12:15:30 +0530

    generator1

--- file/gen.py
+++ file/gen.py
@@ -1,3 +1,21 @@
+import time
+def fib(mymax):
+    a,b=0,1
+    while True:
+        c=a+b
+        if c<mymax:
+            yield c
+            a=b
+            b=c
+        else:
+            break
+gen=fib(10)
+time.sleep(4)
+for i in gen:
+    print(i)
+
+
+
 # def simpleGenfun():
 #     yield 1           
 #     yield 2                    
